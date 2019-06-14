158946819b184424569a97835b2a7063e21099f9
jason <itlanguege@163.com>
2019-06-14 19:52:58 +0800

    更新Demo

--- app/src/main/java/me/jason/imagepicker/MainActivity.java
+++ app/src/main/java/me/jason/imagepicker/MainActivity.java
@@ -1,7 +1,15 @@
 package me.jason.imagepicker;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startImagePicker(View view) {
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(new Observer<Boolean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Boolean aBoolean) {
+                        if (aBoolean) {
+                            ImagePicker.from(MainActivity.this)
+                                    .choose(MimeType.ofAll())
+                                    .imageEngine(new Glide4Engine())
+                                    .forResult(101);
+                        } else {
+                            Toast.makeText(MainActivity.this, "需要相应的权限", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
